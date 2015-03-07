df1c168dae2d9729de9401c585ba25dbd8b4fc28
fr2i972 <13fg04rs17>
2015-03-07 19:52:43 +0100

    commit intermediaire

--- TP1/TP1/TP1/Program.cs
+++ TP1/TP1/TP1/Program.cs
@@ -8,8 +8,105 @@ namespace TP1
 {
     class Program
     {
+
         static void Main(string[] args)
         {
+            #region init value
+            int nombreEntree = 0;
+            int nombreThread = 0;
+            Console.WriteLine("Number entry ?");
+            nombreEntree = int.Parse(Console.ReadLine());
+            // Console.WriteLine("Number thread ?");
+            // numberThread = int.Parse(Console.ReadLine());
+            #endregion
+
+            long startTime = DateTime.Now.Millisecond;
+            Double[] monTab = new Double[nombreEntree];
+            Function.Initialiser(monTab);
+            Function.Trier(monTab);
+            //   Function.Afficher(monTab);
+            long endTime = DateTime.Now.Millisecond;
+
+            long timeAction = endTime - startTime;
+            Console.WriteLine("execution time for  " + nombreEntree + " entry = " + timeAction + " ms");
+
+            Console.WriteLine("min");
+            Console.WriteLine(monTab[0]);
+            Console.WriteLine(monTab[monTab.Length - 1]);
+            Console.WriteLine("max");
+            Console.ReadLine();
         }
     }
+    class Function
+    {
+        public static void Afficher(double[] tableau)
+        {
+            for (int i = 0; i < tableau.Length; i++)
+                Console.WriteLine("T[{0}] = {1}", i, tableau[i]);
+        }
+        public static void Initialiser(double[] tableau)
+        {
+
+            Random r = new Random((int)DateTime.Now.Ticks);
+
+            for (int i = 0; i < tableau.Length; i++)
+
+                tableau[i] = r.NextDouble();
+        }
+
+        public static void Trier(double[] tableau)
+        {
+            TriRapideSequentiel(tableau, 0, tableau.Length - 1);
+        }
+        private static void TriRapideSequentiel(double[] tableau, int debut, int fin)
+        {
+
+            if (debut < fin)
+            {
+
+                int indicePivot = Segmenter(tableau, debut, fin);
+
+                TriRapideSequentiel(tableau, debut, indicePivot - 1);
+
+                TriRapideSequentiel(tableau, indicePivot + 1, fin);
+
+            }
+
+        }
+
+        private static int Segmenter(double[] tableau, int debut, int fin)
+        {
+
+            double valeurPivot = tableau[debut];
+
+            int d = debut + 1;
+
+            int f = fin;
+
+            while (d < f)
+            {
+
+                while (d < f && tableau[f] > valeurPivot)
+                    f--;
+                while (d < f && tableau[d] <= valeurPivot)
+                    d++;
+                double temp = tableau[d];
+
+                tableau[d] = tableau[f];
+
+                tableau[f] = temp;
+
+            }
+
+            if (tableau[d] > valeurPivot)
+                d--;
+            tableau[debut] = tableau[d];
+
+            tableau[d] = valeurPivot;
+
+            return d;
+
+        }
+    }
+
 }
